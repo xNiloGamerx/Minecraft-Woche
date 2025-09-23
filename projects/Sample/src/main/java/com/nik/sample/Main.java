d15ec044511d6eda32c20c276a2f5ad5f0fbce63
NiloGamer <nikb0322@gmail.com>
2025-09-23 22:02:52 +0200

    Added inv command

---
+++ projects/Sample/src/main/java/com/nik/sample/Main.java
@@ -0,0 +1,44 @@
+package com.nik.sample;
+
+import com.nik.sample.commands.InvCommand;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    private static Main instance;
+    private Component prefix;
+    private MiniMessage mm = MiniMessage.miniMessage();
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        instance = this;
+        prefix = mm.deserialize("<gradient:#1d7001:#258f01:#52c234>[Sample]</gradient>" + " ");
+
+        PluginManager pluginManager = this.getServer().getPluginManager();
+
+        InvCommand invCommand = new InvCommand();
+        this.getCommand("inv").setExecutor(invCommand);
+        pluginManager.registerEvents(invCommand, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public MiniMessage getMiniMessage() {
+        return mm;
+    }
+
+    public Component getPrefix() {
+        return prefix;
+    }
+}
